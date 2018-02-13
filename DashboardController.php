3b600716f43aafbfd2527006abf1aa9dc61f7d5d
Doron Eitan <32599090+doreitan@users.noreply.github.com>
2018-02-13 15:40:49 +0100

    Add files via upload

---
+++ DashboardController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+namespace App\Http\Controllers;
+
+
+    
+
+use App\post;
+use App\user;
+use DB;
+class DashboardController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+        $user_id=auth()->user()->id;
+        $user=user::find($user_id);
+
+        return view ('dashboard')->with('posts',$user->posts);
+
+        
+    }
+}
